ea99bec7457311bbf34d91e76d267b9a83b83abd
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2019-04-17 12:01:23 +0530

    First commit

---
+++ fastlane_demo/android/app/src/main/kotlin/com/v2solutions/fastlane_demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.v2solutions.fastlane_demo
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
